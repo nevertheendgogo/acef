a9143488456b360132926f2dc1a1df3f537a497a
xiaohua <1711591690@qq.com>
2020-01-29 14:10:22 +0800

    first commit

---
+++ back/static/js/chunk-2d0aab49.ecb947fa.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0aab49"],{"11c6":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},c=[],r=t("2877"),a={},l=Object(r["a"])(a,u,c,!1,null,null,null);n["default"]=l.exports}}]);
